3b2cd43b783ac8b4f93cd3293f6f3ea8326d2612
David <46406163y@iespoblenou.org>
2017-04-15 20:14:09 +0200

    Creació del adapter i el menu settings

--- app/src/main/java/com/example/user/musicrecorded/MainActivityFragment.java
+++ app/src/main/java/com/example/user/musicrecorded/MainActivityFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -21,6 +27,16 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvPelis = (ListView) view.findViewById(R.id.lvDisco);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_discos_row,
+                R.id.tvDisco,
+                items
+        );
+        lvPelis.setAdapter(adapter);
+
 
         return view;
     }
