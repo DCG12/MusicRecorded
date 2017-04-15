e88107501d13c4b5738d940ba9245d3de22ab46e
David <46406163y@iespoblenou.org>
2017-04-15 19:58:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/musicrecorded/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.user.musicrecorded;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvPelis = (ListView) view.findViewById(R.id.lvDisco);
+
+        return view;
+    }
+}
